ee2dbabcd4d2ee4db0b50ec9f90a6758bb473a47
Kenji Hisazumi <hisazumi@gmail.com>
2019-02-25 14:27:42 +0900

    added some test cases for leds

--- memmapio.c
+++ memmapio.c
@@ -1,10 +1,31 @@
 #include "init.h"
 #include "encoding.h"
 
-#define MU500_BASE         0x04000004
+#define MU500        ((volatile unsigned char *)0x04000000)
 
 int main()
 {
-    *((volatile unsigned char *)MU500_BASE) = 10;
-    while(1);
+    // 7seg test
+    MU500[0] = 0xff;
+    MU500[1] = 0x00;
+    MU500[2] = 0x06;
+    MU500[3] = 0x5b;
+    MU500[4] = 0x4f;
+    MU500[5] = 0x66;
+    MU500[6] = 0x6d;
+    MU500[7] = 0xfc;
+    MU500[8] = 0x55;
+    MU500[0x3f] = 0x55;
+
+    // led test
+    MU500[0x40] = 0xaa;
+    MU500[0x41] = 0x55;
+    MU500[0x42] = 0x00;
+    MU500[0x43] = 0xff;
+    MU500[0x44] = 0xf0;
+    MU500[0x45] = 0x0f;
+    MU500[0x46] = 0x0f;
+    MU500[0x47] = 0x0f;
+
+    while (1);
 }
